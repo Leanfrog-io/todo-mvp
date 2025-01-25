148d242dc25dd4e398fe9db38102509a6aeee39e
Jaakko Karvonen <james@leanfrog.io>
2025-01-25 22:35:55 +0200

    Move components into components folder

---
+++ components/index.js
@@ -0,0 +1,6 @@
+// index.js
+export { default as Checklist } from "./Checklist";
+export { default as CurrentTask } from "./CurrentTask";
+export { default as CurrentWeek } from "./CurrentWeek";
+export { default as Navbar } from "./Navbar";
+export { default as TodoList } from "./TodoList";
